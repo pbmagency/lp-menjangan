920c1116945422d12b77d99efe465828e43f97e6
Mohamad Arif <125123688+mohamadarif03@users.noreply.github.com>
2026-08-19 21:40:06 +0700

    Fix analytics tracking and update hero blockquote

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->validateCsrfTokens(except: ['/analytics/track']);
 
         $middleware->web(append: [
             CacheLandingPage::class,
